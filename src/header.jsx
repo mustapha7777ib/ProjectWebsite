35f2fbcd6d121a27dfcf63df7d9a5231744e41e1
Mustapha Ibrahim <ibrahimmustapha12345678@gmail.com>
2025-03-25 12:41:12 +0100

    ok

--- src/header.jsx
+++ src/header.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import Body from './body';
+import { useState } from 'react';
+
 function Header() {
+    const [show, setshow] = useState(true);
+    const handleclick = () => {
+      setshow(false);
+  };
     return (
       <>
       <div className='header'>
-        <div className='header1'>
-            <p>Home</p>
-            <p>Services</p>
-            <p>About Us</p>
-            <p>Contact Us</p>
+        <div className={`${show ? 'header1' : 'header11'}`}>
+            <p className='buttonss' onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}>Home</p>
+            <p className='buttonss' onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}>Services</p>
+            <p className='buttonss' onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}>About Us</p>
+            <p className='buttonss' onClick={() => window.scrollTo({ top: 1000, behavior: "smooth" })}>Contact Us</p>
         </div>
-        <div className='header2'>
+        <div onClick={() => handleclick()} className='hamburger'>
+            ☰
+        </div>
+        <div className={`${show ? 'header2' : 'header21'}`}>
             <p>Log in</p>
             <p>Sign Up</p>
         </div>
       </div>
+      <Body/>
       </>
     )
   }
